f14a78912ebc6b165204527e730b5d633150934a
turboaux <jje.uribe@gmail.com>
2026-08-29 21:47:57 -0600

    add handlebars  template engine configuration

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// View engine configuration
+const viewsPath = path.join(__dirname, 'views', 'templates')
+const partialsPath = path.join(__dirname, 'views', 'partials')
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Static assets configuration
+const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 app.get('/', (request, response) => {
-  response.send('Hola Mundo')
+  response.render('index', {
+    title: 'Weather App'
+  })
 })
 
 module.exports = app
